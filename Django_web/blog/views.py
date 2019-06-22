1025c282b4d7f142e4e449f15c7cbc832287031c
Saloni Tyagi <saloni19tyagi@gmail.com>
2019-06-22 13:17:00 +0530

    Adding Post to database

--- Django_web/blog/views.py
+++ Django_web/blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [ 
-	{ 
-		'author' : 'Saloni Tyagi' ,
-		'title'  : 'Blog Post 1' ,
-		'Date_posted' : '20 june 2019',
-		'content' : 'First Blog Post'
-	} ,
-	{
-		'author' : 'Yukta Anand',
-		'title' : 'Blog Post 2' ,
-		'Date_posted' : '21 june 2019',
-		'content' : 'Second Blog Post'
-
-	}
-]
-
+from .models import Post
 
 def home(request):
 	context = {
-		'posts' : posts 
+		'posts' : Post.objects.all()
 	}
 	return render(request , 'blog/home.html/', context)
 
